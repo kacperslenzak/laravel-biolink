5aa05f9a30ccab9ffe4426e6f14e068cdf50cd21
Kacper Slenzak <kacperinos@gmail.com>
2024-06-16 21:08:45 +0100

    change cant remember what

--- app/Providers/Filament/AccountPanelProvider.php
+++ app/Providers/Filament/AccountPanelProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers\Filament;
 
 use App\Filament\Pages\Login;
 use App\Filament\Pages\Register;
+use Filament\Enums\ThemeMode;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -33,6 +34,8 @@ class AccountPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->defaultThemeMode(ThemeMode::Dark)
+            ->brandName('fraud.cool')
             ->viteTheme('resources/css/filament/account/theme.css')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -9,13 +9,14 @@
         <div class="profile">
             <h1>{{ $user->name }}</h1>
 
-            <div class="icons mt-3">
+            <div class="icons mt-3 text-center">
                 @foreach($links as $link)
-                    <a href="//{{$link->link_url}}">
+                    <a href="//{{$link->link_url}}" target="_blank">
                         <i class="{{ $link->icon_class() }} text-white"></i>
                     </a>
                 @endforeach
             </div>
+
         </div>
     </div>
 @endsection
