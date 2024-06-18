0b078374fef24324a189593c2051faa54a93e3d9
Kacper <kacperinos@gmail.com>
2024-06-19 00:51:01 +0100

    Changes to profile settings?

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel, { refreshPaths } from 'laravel-vite-plugin'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/sass/app.scss', 'resources/js/app.js'],
+            input: ['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/filament/account/theme.css'],
             refresh: [
                 ...refreshPaths,
                 'app/Filament/**',
