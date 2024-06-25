b5eb3342e597f7de07b3222168ff6c2c5e54b6de
Kacper Slenzak <kacperinos@gmail.com>
2024-06-25 02:24:25 +0100

    fix no user redirect error

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,11 +13,13 @@ class UserController extends Controller
     public function show(string $username): mixed
     {
         $user = User::where('name', $username)->first();
-        $links = Links::all()->where('user_id', $user->id);
-        $profile_settings = ProfileSettings::all()->where('user_id', $user->id)->first();
+
         if(is_null($user)){
             return redirect('/');
         }
+
+        $links = Links::all()->where('user_id', $user->id);
+        $profile_settings = ProfileSettings::all()->where('user_id', $user->id)->first();
 
         return view('user.profile', [
             'user' => $user,
