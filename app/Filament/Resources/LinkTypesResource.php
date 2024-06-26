690b2f4821c21f26cd523efe4ce6387f198d637d
Kacper <kacperinos@gmail.com>
2024-06-26 21:35:09 +0100

    Add users resource and group admin pages

--- app/Filament/Resources/LinkTypesResource.php
+++ app/Filament/Resources/LinkTypesResource.php
@@ -22,6 +22,8 @@ class LinkTypesResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationGroup = 'Admin';
+
     public static function form(Form $form): Form
     {
         return $form
